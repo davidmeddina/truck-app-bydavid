a70296100782c5d1dbf0cc83b46955723169f844
david medina <davidmeddina@gmail.com>
2019-04-08 20:14:27 -0500

    Flash Alerts Helper working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  
+  def bootstrap_class_for(flash_type)
+    {
+      succes: "alert-success",
+      error:  "alert-danger",
+      alert: "alert-warning",
+      notice: "alert-primary"
+    }[flash_type.to_sym] || flash_type.to_s
+   end
+  
+  def flash_messages(_opts = {})
+    flash.each do |msg_type, message|
+      next if msg_type == :form_error
+      concat(content_tag(:div, message.html_safe,{
+        class: "alert #{bootstrap_class_for(msg_type)} fade in"}) do
+        concat content_tag(:button, 'x', class: "close", data: {
+          dismiss: 'alert'
+        })
+        concat message.html_safe
+        end)
+     end 
+     nil
+  end
 end
