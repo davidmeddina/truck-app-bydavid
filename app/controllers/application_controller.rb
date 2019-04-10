e9f985e2b559dda833b3e42d6816caf4db7384eb
david medina <davidmeddina@gmail.com>
2019-04-09 19:02:06 -0500

    Roles added to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  ## add parameters/column to Devise
+  before_action :add_parameters_to_user, if: :devise_controller?
+
+  
+  protected
+    def add_parameters_to_user
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:role])
+    end
 end
